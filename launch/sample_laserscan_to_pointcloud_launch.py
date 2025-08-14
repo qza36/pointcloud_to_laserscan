73c3f66a56237cf5ffe9c7c7a949b73d0f91cddb
ZhiangQi <an9qi@yandex.com>
2025-08-14 15:23:11 +0800

    fix:修复QoS

--- launch/sample_laserscan_to_pointcloud_launch.py
+++ launch/sample_laserscan_to_pointcloud_launch.py
@@ -1,10 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import ExecuteProcess
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
-import yaml
 
 
 def generate_launch_description():
@@ -13,35 +9,24 @@ def generate_launch_description():
             name='scanner', default_value='scanner',
             description='Namespace for sample topics'
         ),
-        ExecuteProcess(
-            cmd=[
-                'ros2', 'topic', 'pub', '-r', '10',
-                '--qos-profile', 'sensor_data',
-                [LaunchConfiguration(variable_name='scanner'), '/scan'],
-                'sensor_msgs/msg/LaserScan', yaml.dump({
-                    'header': {'frame_id': 'scan'}, 'angle_min': -1.0,
-                    'angle_max': 1.0, 'angle_increment': 0.1, 'range_max': 10.0,
-                    'ranges': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-                })
-            ],
-            name='scan_publisher'
-        ),
         Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments=[
-                '--x', '0', '--y', '0', '--z', '0',
-                '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
-                '--frame-id', 'map', '--child-frame-id', 'scan'
-            ]
-        ),
-        Node(
-            package='pointcloud_to_laserscan',
-            executable='laserscan_to_pointcloud_node',
-            name='laserscan_to_pointcloud',
-            remappings=[('scan_in', [LaunchConfiguration(variable_name='scanner'), '/scan']),
-                        ('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-            parameters=[{'target_frame': 'scan', 'transform_tolerance': 0.01}]
-        ),
+            package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
+            remappings=[('cloud_in',  ['/livox/idar/pointcloud']),
+                        ('scan',  ['/scan'])],
+            parameters=[{
+                'target_frame': 'livox_frame',
+                'transform_tolerance': 0.01,
+                'min_height': -1.0,
+                'max_height': 0.1,
+                'angle_min': -3.14159,  # -M_PI/2
+                'angle_max': 3.14159,  # M_PI/2
+                'angle_increment': 0.0043,  # M_PI/360.0
+                'scan_time': 0.3333,
+                'range_min': 0.45,
+                'range_max': 10.0,
+                'use_inf': True,
+                'inf_epsilon': 1.0
+            }],
+            name='pointcloud_to_laserscan'
+        )
     ])
--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -6,41 +6,21 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='scanner', default_value='scanner',
-            description='Namespace for sample topics'
-        ),
-        Node(
-            package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
-            remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-            parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
-            name='cloud_publisher'
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments=[
-                '--x', '0', '--y', '0', '--z', '0',
-                '--qx', '0', '--qy', '0', '--qz', '0', '--qw', '1',
-                '--frame-id', 'map', '--child-frame-id', 'cloud'
-            ]
-        ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
-                        ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan'])],
+            remappings=[('cloud_in',  '/rslidar_points'),
+                        ('scan', '/scan')],
             parameters=[{
-                'target_frame': 'cloud',
+                'target_frame': 'base_link',
                 'transform_tolerance': 0.01,
                 'min_height': 0.0,
-                'max_height': 1.0,
+                'max_height': 0.65,
                 'angle_min': -1.5708,  # -M_PI/2
                 'angle_max': 1.5708,  # M_PI/2
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 0.45,
-                'range_max': 4.0,
+                'range_min': 0.05,
+                'range_max': 5.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
